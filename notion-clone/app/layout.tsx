22dd2e692e784a1c5debb4f76608675e044d3aa9
GenkiKishikawa <gen-at86.battleship.930@docomo.ne.jp>
2025-05-16 01:26:14 +0900

    Add: ダークモード追加

--- notion-clone/app/layout.tsx
+++ notion-clone/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -37,11 +38,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="jotion-theme-2"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
