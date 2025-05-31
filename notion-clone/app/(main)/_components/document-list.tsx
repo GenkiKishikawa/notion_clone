222bb715955592215e7b5a598c52dc008c09e23e
GenkiKishikawa <gen-at86.battleship.930@docomo.ne.jp>
2025-05-31 12:29:40 +0900

    Add: Document List追加

---
+++ notion-clone/app/(main)/_components/document-list.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import { useRouter, useParams } from "next/navigation";
+import { useState } from "react";
+
+interface DocumentListProps {
+  parentDocumentId?: Id<"documents">;
+  level?: number;
+  data?: Doc<"documents">[];
+}
+
+export const DocumentList = ({
+  parentDocumentId,
+  level = 0,
+}: DocumentListProps) => {
+  const params = useParams();
+  const router = useRouter();
+  const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+
+  const onExpand = (documentId: string) => {
+    setExpanded((prevExpanded) => ({
+      ...prevExpanded,
+      [documentId]: !prevExpanded[documentId],
+    }));
+  };
+
+  const documents = useQuery();
+
+  return <div>Document List</div>;
+};
--- notion-clone/app/(main)/_components/navigation.tsx
+++ notion-clone/app/(main)/_components/navigation.tsx
@@ -10,7 +10,7 @@ import {
 import { usePathname } from "next/navigation";
 import React, { ElementRef, use, useRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
-import { useQuery, useMutation } from "convex/react";
+import { useMutation } from "convex/react";
 import { toast } from "sonner";
 
 import { cn } from "@/lib/utils";
@@ -22,7 +22,6 @@ import { Item } from "./item";
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
@@ -143,9 +142,7 @@ export const Navigation = () => {
           <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
