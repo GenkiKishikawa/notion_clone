c594f9185c0d78f3de6cab0b0c6571ce1af3e409
GenkiKishikawa <gen-at86.battleship.930@docomo.ne.jp>
2025-05-31 15:39:44 +0900

    Add: Document List追加

--- notion-clone/app/(main)/_components/document-list.tsx
+++ notion-clone/app/(main)/_components/document-list.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { Doc, Id } from "@/convex/_generated/dataModel";
 import { useRouter, useParams } from "next/navigation";
 import { useState } from "react";
+import { useQuery } from "convex/react";
+import { cn } from "@/lib/utils";
+import { FileIcon } from "lucide-react";
+
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
 
 interface DocumentListProps {
   parentDocumentId?: Id<"documents">;
@@ -25,7 +31,63 @@ export const DocumentList = ({
     }));
   };
 
-  const documents = useQuery();
+  const documents = useQuery(api.documents.getSidebar, {
+    parentDocument: parentDocumentId
+  });
 
-  return <div>Document List</div>;
+  const onRedirect = (documentId: string) => {
+    router.push('/documents/${documentId}');
+  };
+
+  if (documents === undefined) {
+    return (
+      <>
+        <Item.Skeleton level={level} />
+        {level === 0 && (
+          <>
+            <Item.Skeleton level={level} />
+            <Item.Skeleton level={level} />
+          </>
+        )}
+      </>
+    )
+  }
+
+  return (
+    <>
+      <p
+        style={{
+          paddingLeft: level ? `${(level * 12) + 25}px` : undefined,
+        }}
+        className={cn(
+          "hidden text-sm font-medium text-muted-foreground/80",
+          expanded && "last:block",
+          level === 0 && "hidden"
+        )}
+      >
+        No pages inside
+      </p>
+      {documents.map((document) => (
+        <div key={document._id}>
+          <Item
+            id={document._id}
+            onClick={() => onRedirect(document._id)}
+            label={document.title}
+            icon={FileIcon}
+            documentIcon={document.icon}
+            active={params.documentId === document._id}
+            level={level}
+            onExpand={() => onExpand(document._id)}
+            expanded={expanded[document._id]}
+          />
+          {expanded[document._id] && (
+            <DocumentList
+              parentDocumentId={document._id}
+              level={level + 1}
+            />
+          )}
+        </div>
+      ))}
+    </>
+  );
 };
--- notion-clone/app/(main)/_components/navigation.tsx
+++ notion-clone/app/(main)/_components/navigation.tsx
@@ -18,6 +18,7 @@ import { api } from "@/convex/_generated/api";
 
 import { UserItem } from "./user-item";
 import { Item } from "./item";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathname = usePathname();
