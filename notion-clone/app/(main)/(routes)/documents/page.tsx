916ce5912fde07ec3b2525f004eb09c99f45dcd6
GenkiKishikawa <gen-at86.battleship.930@docomo.ne.jp>
2025-06-11 19:11:02 +0900

    Add: Editor Component追加

--- notion-clone/app/(main)/(routes)/documents/page.tsx
+++ notion-clone/app/(main)/(routes)/documents/page.tsx
@@ -1,20 +1,24 @@
 "use client";
 
 import Image from "next/image";
-import { useUser } from "@clerk/clerk-react";
 import { PlusCircle } from "lucide-react";
 import { useMutation } from "convex/react";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
 
 const DocumentsPage = () => {
+  const router = useRouter();
   const { user } = useUser();
   const create = useMutation(api.documents.create);
 
   const onCreate = () => {
-    const promise = create({ title: "Untitled" });
+    const promise = create({ title: "Untitled" }).then((documentId) => {
+      router.push(`/documents/${documentId}`);
+    });
 
     toast.promise(promise, {
       loading: "Creating a new note...",
